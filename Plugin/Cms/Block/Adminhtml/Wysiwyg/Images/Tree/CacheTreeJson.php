7c8b895d1d4ec8a16368a70a1acc40b5f9ef1342
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2019-06-14 10:19:51 +0000

    [HAAR-1666] Fix problem with cache only root level

--- Plugin/Cms/Block/Adminhtml/Wysiwyg/Images/Tree/CacheTreeJson.php
+++ Plugin/Cms/Block/Adminhtml/Wysiwyg/Images/Tree/CacheTreeJson.php
@@ -4,22 +4,40 @@ namespace MageSuite\MediaListingCache\Plugin\Cms\Block\Adminhtml\Wysiwyg\Images\
 
 class CacheTreeJson
 {
+
+    const ONE_DAY = 86400;
+    const TREE_JSON_TAG = 'tree_json';
+
+    /**
+     * @var \Magento\Framework\App\RequestInterface
+     */
+    protected $request;
+
+    /**
+     * @var \Magento\Cms\Helper\Wysiwyg\Images
+     */
+    protected $imagesHelper;
+
     /**
      * @var \MageSuite\MediaListingCache\Model\Cache\Type\MediaListing
      */
     protected $cache;
 
-    const ONE_DAY = 86400;
-    const TREE_JSON_TAG = 'tree_json';
-
-    public function __construct(\MageSuite\MediaListingCache\Model\Cache\Type\MediaListing $cache)
+    public function __construct(
+        \Magento\Framework\App\RequestInterface $request,
+        \Magento\Cms\Helper\Wysiwyg\Images $imagesHelper,
+        \MageSuite\MediaListingCache\Model\Cache\Type\MediaListing $cache
+    )
     {
         $this->cache = $cache;
+        $this->imagesHelper = $imagesHelper;
+        $this->request = $request;
     }
 
     public function aroundGetTreeJson(\Magento\Cms\Block\Adminhtml\Wysiwyg\Images\Tree $subject, callable $proceed)
     {
-        $cacheKey = self::TREE_JSON_TAG;
+        $nodeName = $this->request->getParam($this->imagesHelper->getTreeNodeName());
+        $cacheKey = sprintf('%s_%s', self::TREE_JSON_TAG, md5($nodeName));
 
         $json = $this->cache->load($cacheKey);
 
